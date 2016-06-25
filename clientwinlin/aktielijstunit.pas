0742bfa02cd080fe6ee14a2b34dbf207a8dddaee
chrisgradussen <chrisgradussen@jumboduiven.nl>
2016-06-25 10:30:16 +0200

    Wat wijzingen in reclame

--- clientwinlin/aktielijstunit.pas
+++ clientwinlin/aktielijstunit.pas
@@ -7,7 +7,7 @@ interface
 uses
   Classes, SysUtils, FileUtil, LR_Class, LR_DBSet, ZDataset, rxdbgrid,
   LResources, Forms, Controls, Graphics, Dialogs, ExtCtrls, Calendar, StdCtrls,
-  db, DateUtils;
+  db, DateUtils, strutils;
 
 type
 
@@ -64,8 +64,37 @@ begin
 end;
 
 procedure TFormAktielijst.AfdrukkenButtonClick(Sender: TObject);
+var
+    i : integer;
+    s,sql1,sql2 : string;
 begin
-  frreport1.ShowReport;
+  sql1 := 'SELECT max(r.VOORDEEL) as aktieomschrijving,a.X_ARTIKELNUMMER,max(a.X_NAAM) as omschrijving ,max(a.X_VERKOOPPRIJS) as reclameprijs,'+
+' max(begindatum) startdatum, max(einddatum) as einddatum FROM RECLAME_PROCEDURE(:BEGINDATUM,:STOPDATUM) r join artikel a on r.artikel_eanupc = a.EANUPC where r.begindatum <=  :STOPDATUM and  r.einddatum >= :BEGINDATUM and a.X_SUBGROEP_ID in (SELECT r.SUBGROEP_ID FROM GROEP r where r.BESTELGROEP_ID in :GROEPEN) group by a.X_ARTIKELNUMMER';
+
+  zaktielijstquery.Close;
+  zaktielijstquery.ParamByName('BEGINDATUM').AsDate:=  calendarstart.DateTime;
+  zaktielijstquery.ParamByName('STOPDATUM').AsDate:=  calendarstop.DateTime;
+  if rxdbgrid1.SelectedRows.Count > 0 then
+  begin
+    s := '(';
+    for i := 0 to rxdbgrid1.SelectedRows.Count -1 do
+    begin
+      rxdbgrid1.DataSource.DataSet.GotoBookmark(rxdbgrid1.selectedrows.Items[i]);
+      s := concat(s,(rxdbgrid1.datasource.DataSet.FieldByName('id').asstring));
+      if i < rxdbgrid1.selectedrows.count -1 then
+        s := concat(s,',');
+    end;
+    s := concat(s,')');
+
+
+
+  sql1 := replacestr(sql1,':GROEPEN',s);
+
+    zaktielijstquery.SQL.Text := sql1;
+
+    zaktielijstquery.Open;
+    frreport1.ShowReport;
+  end;
 end;
 
 procedure TFormAktielijst.SluitenButtonClick(Sender: TObject);
